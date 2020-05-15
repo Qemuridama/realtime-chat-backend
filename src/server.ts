af55354af9901301326b2c5408ae781cd9d73253
Felipe Silva <felipe.dacs@gmail.com>
2020-05-15 01:05:22 -0300

    feat: cria conexão inicial com websocket
    Ainda falta ver como conectar com cors
    
    Co-authored-by: Laks Castro <lakhsmycastro336@gmail.com>
    Co-authored-by: castroldp@outlook.com <castroldp@outlook.com>
    Co-authored-by: Bruno Ramires <brunormferreira@hotmail.com>
    Co-authored-by: Emmanuel <emmanuel_costasilva@hotmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,80 @@
+import express from 'express';
+import path from 'path';
+import http from 'http';
+import socket from 'socket.io';
+import cors from 'cors';
+
+const app = express();
+app.use(cors());
+
+const server = http.createServer(app);
+const io = socket(server);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '..', 'frontend', 'index.html'));
+});
+
+
+// TODO
+io.origins(['*']);
+//io.origins('*:*') 
+//io.origins('*')
+//io.set('origins', '*:*');
+
+
+interface MessageInterface {
+  author: string
+  date: Date
+  content: string
+}
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (message: MessageInterface) => {
+    console.log(`${message.author} says: ${message.content}`);
+    io.emit('chat message', message);
+  });
+});
+
+server.listen(3333, () => {
+  console.log('listening on *:3333');
+});
+
+
+/*
 import express from 'express'
+import server from 'http'
+import socket from 'socket.io'
+import cors from 'cors';
 
 const app = express()
 
+app.use(cors({origin: '*'}));
+
+const io = socket(server.createServer(app))
+
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello world' })
+    return response.json({ message: 'Hello world' })
 })
 
-app.listen(3333, () => {
-  console.log('SERVER STARTED ON PORT 3333')
+interface MessageInterface {
+    author: string
+    date: Date
+    text: string
+}
+
+let messages: MessageInterface[];
+
+io.on('connection', socket => {
+    console.log('Socket ID: ' + socket.id)
+
+    io.on('sendMessage', (message: MessageInterface) => {
+        messages.push(message);
+        socket.broadcast.emit('receivedMessage', message);
+    });
 })
+
+app.listen(3333, () => console.log('Server started on port 3333'))
+*/

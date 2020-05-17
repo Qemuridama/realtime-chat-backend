8e5f14395c1b696bb3315cee3bc254acd91abc4c
LaksCastro <lakhsmycastro336@gmail.com>
2020-05-17 15:37:43 -0300

    feat: storage all message in temp var 'messages'

--- src/server.ts
+++ src/server.ts
@@ -2,35 +2,42 @@ import express from 'express';
 import path from 'path';
 import http from 'http';
 import socket from 'socket.io';
-import cors from 'cors';
 
 const app = express();
 const server = http.createServer(app);
 const io = socket(server);
 
 // cors
-io.origins("*:*")
+io.origins('*:*');
 
-app.get('/', (req, res) => {
+app.get('/', (req: any, res: any) => {
   res.sendFile(path.resolve(__dirname, '..', 'frontend', 'index.html'));
 });
 
 enum EventMessage {
-  NEW_MESSAGE = "new_message"
+  NEW_MESSAGE = 'new_message',
+  INIT_MESSAGES = 'initial_messages',
 }
 
 interface MessageInterface {
-  author: string
-  date: Date
-  content: string
+  author: string;
+  date: Date;
+  content: string;
 }
 
-io.on('connection', (socket) => {
+const messages: MessageInterface[] = [];
+
+io.on('connection', (socket: any) => {
   console.log('a user connected');
 
+  io.emit(EventMessage['INIT_MESSAGES'], messages);
+
   socket.on(EventMessage['NEW_MESSAGE'], (message: MessageInterface) => {
-    message.date = new Date()
-    console.log(`${message.author} says: ${message.content} at ${message.date}`);
+    message.date = new Date();
+    console.log(
+      `${message.author} says: ${message.content} at ${message.date}`
+    );
+    messages.push(message);
     io.emit(EventMessage['NEW_MESSAGE'], message);
   });
 });
